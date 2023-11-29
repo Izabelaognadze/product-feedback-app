17a1f095eebbddcaa1a9070be7f66b2d0b874251
Izabela Ognadze <75525287+Izabelaognadze@users.noreply.github.com>
2023-11-29 20:38:02 +0400

    day 1

--- apps/frontend/src/app/app.routes.ts
+++ apps/frontend/src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Route } from '@angular/router';
+import { ButtonComponent } from './design-system/button/button.component'
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+    {
+        path:'iza', component: ButtonComponent
+    }
+];
+
+// const appRoutes: Route[] = [
+//     {
+//         path:'/', component: ButtonComponent
+//     }
+// ];
+
+// export default appRoutes;
+// @NgModule({
+//     imports: [RouterModule.forRoot(appRoutes)],
+//     exports: [RouterModule]
+//   })
+//   export class AppRoutingModule {}
