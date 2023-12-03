b9d5c94ad57bb0ee4c97613253fb65fb4e95ca45
Izabela Ognadze <75525287+Izabelaognadze@users.noreply.github.com>
2023-12-04 01:04:37 +0400

    combo box update

--- apps/frontend/src/app/design-system/combo-box/combo-box.component.ts
+++ apps/frontend/src/app/design-system/combo-box/combo-box.component.ts
@@ -1,20 +1,27 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { v4 as uuidv4} from 'uuid'
+import { v4 as uuidv4} from 'uuid';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'product-feedback-app-combo-box',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './combo-box.component.html',
   styleUrl: './combo-box.component.css'
 })
 export class ComboBoxComponent{
+  selectedSort: string = 'Most Upvotes';
+
   sort = [
     {id : uuidv4(), sort : 'Most Upvotes'},
     {id : uuidv4(), sort : 'Least Upvotes'},
     {id : uuidv4(), sort : 'Most Comments'},
     {id : uuidv4(), sort : 'Least Comments'},
   ]
+
+  onSortChange(event: Event) {
+    this.selectedSort = (event.target as HTMLSelectElement).value;
+  }
 
 }
