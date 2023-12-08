b09c009ef534e92a8dd16272f7c58bb40f842af4
Izabela Ognadze <75525287+Izabelaognadze@users.noreply.github.com>
2023-12-09 01:36:48 +0400

    suggestion page update

---
+++ apps/frontend/src/app/suggestions/roadmap/roadmap.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'product-feedback-app-roadmap',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './roadmap.component.html',
+  styleUrl: './roadmap.component.css',
+})
+export class RoadmapComponent {}
--- apps/frontend/src/app/suggestions/suggestions.component.ts
+++ apps/frontend/src/app/suggestions/suggestions.component.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import {FeedbackBoardComponent} from '../suggestions/feedback-board/feedback-board.component';
 import {HeaderComponent} from './header/header.component';
 import { NoFeedbackComponent } from './no-feedback/no-feedback.component'
+import { RoadmapComponent } from '../roadmap/roadmap.component';
+import { SelectorButtonComponent } from '../design-system/selector-button/selector-button.component';
 
 @Component({
   selector: 'product-feedback-app-suggestions',
   standalone: true,
-  imports: [CommonModule, FeedbackBoardComponent, HeaderComponent, NoFeedbackComponent],
+  imports: [CommonModule, FeedbackBoardComponent, HeaderComponent, NoFeedbackComponent, RoadmapComponent, SelectorButtonComponent],
   templateUrl: './suggestions.component.html',
   styleUrl: './suggestions.component.css',
 })
