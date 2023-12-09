876a258296c00c3213cd38b32d9d3487a7dc0171
Izabela Ognadze <75525287+Izabelaognadze@users.noreply.github.com>
2023-12-10 00:03:56 +0400

    roadmap update

--- apps/frontend/src/app/suggestions/roadmap/roadmap.component.ts
+++ apps/frontend/src/app/suggestions/roadmap/roadmap.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ButtonDirective } from '../../design-system/button.directive';
 
 @Component({
   selector: 'product-feedback-app-roadmap',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ButtonDirective],
   templateUrl: './roadmap.component.html',
   styleUrl: './roadmap.component.css',
 })
